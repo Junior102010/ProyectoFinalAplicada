37d57012e5cd66af21242ab909d5079be4024648
Ramon Junior Urena Vasquez <ramon_urena2@ucne.edu.do>
2025-11-28 13:24:57 -0400

    cambio5

---
+++ ProyectoFinalAplicada1/Migrations/20251128172435_Arreglos.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoFinalAplicada1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Arreglos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoriaId",
+                table: "Proveedor",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Categoria",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categoria_Proveedor_CategoriaId",
+                table: "Categoria",
+                column: "CategoriaId",
+                principalTable: "Proveedor",
+                principalColumn: "ProveedorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categoria_Proveedor_CategoriaId",
+                table: "Categoria");
+
+            migrationBuilder.DropColumn(
+                name: "CategoriaId",
+                table: "Proveedor");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoriaId",
+                table: "Categoria",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
